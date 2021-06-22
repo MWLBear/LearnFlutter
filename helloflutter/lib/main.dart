e7cd15a55380e6837adf0c3c67bf2b979525df94
MWLBear <771646799@qq.com>
2021-06-22 18:12:15 +0800

    add net_requset

--- helloflutter/lib/main.dart
+++ helloflutter/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-
+import "dart:io";
+import 'package:helloflutter/service/http_request.dart';
 main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,6 +29,38 @@ class MyApp extends StatelessWidget {
 class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('123');
+    return NetTextContent();
   }
 }
+
+class NetTextContent extends StatefulWidget {
+  const NetTextContent({Key? key}) : super(key: key);
+
+  @override
+  _NetTextContentState createState() => _NetTextContentState();
+}
+
+class _NetTextContentState extends State<NetTextContent> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    HttpRequest.requset("https://httpbin.org/get",params: {'name':'why'}).then((value){
+      print(value);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+Future<String> getNetWork(){
+  return Future<String>((){
+    sleep(Duration(seconds: 3));
+    return "net work";
+  });
+}
+
+
---
+++ helloflutter/lib/service/http_request.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+import 'HTTPConfig.dart';
+
+class HttpRequest {
+  static final BaseOptions options = BaseOptions(
+      baseUrl: HTTPConfig.baseURL, connectTimeout: HTTPConfig.timeout);
+  static final Dio dio = Dio(options);
+
+  static Future<T> requset<T> (String url,
+      {String method = 'get', Map<String, dynamic>? params,Interceptor? inter}) async{
+    //请求的单独配置
+    final options = Options(method: method);
+   // 添加一个拦截器
+    Interceptor dInter = InterceptorsWrapper(
+      onRequest: (options,handel){
+        print('拦截了请求');
+        return handel.next(options);
+      },
+      onResponse: (response,handel){
+        print('拦截了响应');
+        return handel.next(response);
+      },
+      onError: (DioError error,handel){
+        return handel.next(error);
+      }
+    );
+    List<Interceptor> inters = [dInter];
+    if(inter != null){
+      inters.add(dInter);
+    }
+    dio.interceptors.addAll(inters);
+    //发送网络请求
+    try {
+      Response response = await dio.request(url,queryParameters:params, options: options);
+      return response.data;
+    }on DioError catch(e){
+      return Future.error(e);
+    } 
+
+  }
+}
