b335b5e8dd293bd87668b4cf0f5ff54e25b13d09
MWLBear <771646799@qq.com>
2021-08-12 15:12:41 +0800

    add flutter_bolc counter demo

---
+++ lianxiproject/lib/状态管理方案/BLoC/flutter_bloc/counter_demo/counter/view/counter_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../counter.dart';
+
+class CounterView extends StatelessWidget {
+  const CounterView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(title: const Text("Counter"),),
+      body: Center(
+        child: BlocBuilder<CounterCubit,int>(
+          builder: (ctx,state){
+            return Text("$state",style: textTheme.headline3,);
+          },
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            key: const Key('counterView_increment_floatingActionButton'),
+            onPressed: ()=> context.read<CounterCubit>().increment(),
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            key: const Key('counterView_decrement_floatingActionButton'),
+            onPressed: ()=> context.read<CounterCubit>().decrement(),
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
